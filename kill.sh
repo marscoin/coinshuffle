3b5672ba0bff4da6f3f56fc566118620275bdc37
Lennart Lopin <novalis78@gmail.com>
2022-01-31 10:48:07 -0500

    some smaller fixes

--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
-pkill -f 'python client.py'
-pkill -f 'python shuffle_server.py'
+pkill -f 'python3 client.py'
+pkill -f 'python3 shuffle_server.py'
--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-c0=http://0.0.0.0:5000/
-c1=http://0.0.0.0:5001/
-c2=http://0.0.0.0:5002/
-python client.py -p 5000 --peers $c1 $c2 &
-python client.py -p 5001 --peers $c0 $c2 &
-python client.py -p 5002 --peers $c0 $c1 &
+c0=http://0.0.0.0:6000/
+c1=http://0.0.0.0:6001/
+c2=http://0.0.0.0:6002/
+python3 client.py -p 6000 --peers $c1 $c2 &
+python3 client.py -p 6001 --peers $c0 $c2 &
+python3 client.py -p 6002 --peers $c0 $c1 &
 sleep 3
-
+echo "Now curl start"
 curl -d '{"source":"alice","target":"b","amount":"500"}' \
   -H "Content-Type: application/json" -X POST \
-  http://0.0.0.0:5002/transactions/new
+  http://0.0.0.0:6002/transactions/new
 
-curl http://0.0.0.0:5002/mine
-curl http://0.0.0.0:5001/resolve
-curl http://0.0.0.0:5001/chain | python -m json.tool
+curl http://0.0.0.0:6002/mine
+curl http://0.0.0.0:6001/resolve
+curl http://0.0.0.0:6001/chain | python3 -m json.tool
 
 sleep 3
 
